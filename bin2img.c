54d15a1a655f12fc36ce3ca873c325a7db0ce3dd
franck dehornoy <franck.dehornoy@selhagroup.com>
2023-04-11 12:20:49 +0200

    Save 11avr23 12:20

--- bin2img.c
+++ bin2img.c
@@ -254,19 +254,43 @@ static void socfpgaimage_set_header(void* ptr, uint32_t data_size)
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	#define FILENAME				"myBoot.bin"
-	#define	OUTPUT_FILENAME			"myBoot.img"
-	#define	OUTPUT_FILENAME_QUAD	"myBootQuad.img"
+	#define DEFAULT_INPUT_FILENAME		"myBoot.bin"
+	#define	DEFAULT_OUTPUT_FILENAME		"myBoot.img"
+	#define	SIZEOF_FILENAME				256
 
-	uint8_t*	ucBuffer			=NULL;
-	uint32_t	uiSize				=0;
-	FILE*		myFile				=0;
-	FILE*		myOutputFile		=0;
-	uint8_t*	ucOriginalBuffer	=NULL;
+	uint8_t*	ucBuffer				=NULL;
+	uint32_t	uiSize					=0;
+	FILE*		myFile					=0;
+	FILE*		myOutputFile			=0;
+	uint8_t*	ucOriginalBuffer		=NULL;
+	char		caInputFileName[SIZEOF_FILENAME];
+	char		caOutputFileName[SIZEOF_FILENAME];
 
-	fopen_s(&myFile, FILENAME, "rb");
+	sprintf_s(caInputFileName,	SIZEOF_FILENAME, DEFAULT_INPUT_FILENAME);
+	sprintf_s(caOutputFileName, SIZEOF_FILENAME, DEFAULT_OUTPUT_FILENAME);
+
+	if (argc >= 2)       // 1er argument (optionnel) est le fichier d'entree 
+	{
+		printf("argv[1]     = %s\n",	argv[1]);
+		printf("argv[1] len = %d\n",	strlen(argv[1]));
+		memset(caInputFileName, 0, SIZEOF_FILENAME);
+		strncpy_s(caInputFileName, SIZEOF_FILENAME, argv[1], strlen(argv[1]));
+	}
+
+	if (argc >= 3)       // 2eme argument (optionnel) est le fichier de sortie
+	{
+		printf("argv[2]     = %s\n", argv[2]);
+		printf("argv[2] len = %d\n", strlen(argv[2]));
+		memset(caOutputFileName, 0, SIZEOF_FILENAME);
+		strncpy_s(caOutputFileName, SIZEOF_FILENAME, argv[2], strlen(argv[2]));
+	}
+
+	// to do - here check input file presence
+
+
+	fopen_s(&myFile, caInputFileName, "rb");
 	if (myFile == NULL)	return -1;
 	fseek(myFile, 0, SEEK_END);
 	uiSize = ftell(myFile);
@@ -278,11 +302,7 @@ int main()
 
 	socfpgaimage_set_header(ucBuffer, uiSize);
 
-	fopen_s(&myOutputFile, OUTPUT_FILENAME, "wb");
-	fwrite(ucOriginalBuffer, 1, PADDED_SIZE, myOutputFile);
-	fclose(myOutputFile);
-
-	fopen_s(&myOutputFile, OUTPUT_FILENAME_QUAD, "wb");
+	fopen_s(&myOutputFile, caOutputFileName, "wb");
 	fwrite(ucOriginalBuffer, 1, PADDED_SIZE, myOutputFile);
 	fwrite(ucOriginalBuffer, 1, PADDED_SIZE, myOutputFile);
 	fwrite(ucOriginalBuffer, 1, PADDED_SIZE, myOutputFile);
